96c1d9b1eac43e62cb5473ff59994a6182f0dee2
Park Yonggwan <pyk@inbus.co.kr>
2023-01-26 16:04:13 +0900

    menulist 조회 시 정렬 추가

--- src/main/java/com/example/demo/data/dao/cmm/impl/MenuDAOImpl.java
+++ src/main/java/com/example/demo/data/dao/cmm/impl/MenuDAOImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.data.dao.cmm.impl;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.data.dao.cmm.MenuDAO;
@@ -18,6 +19,6 @@ public class MenuDAOImpl implements MenuDAO {
 
     @Override
     public List<Menu> selectMenuList() {
-        return menuRepository.findAll();
+        return menuRepository.findAll(Sort.by(Sort.Direction.ASC, "sortId"));
     }
 }
