9c391761852e2a6bc70f6623602ab83a898799cc
Park Yonggwan <pyk@inbus.co.kr>
2023-01-30 14:30:55 +0900

    공통코드조회 테스트 수정

--- src/test/java/com/example/demo/controller/tmm/TMMA0011ControllerTest.java
+++ src/test/java/com/example/demo/controller/tmm/TMMA0011ControllerTest.java
@@ -16,7 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.demo.model.tmm.TmCodexh;
+import com.example.demo.data.dto.cmm.CommonCodeHeaderResponseDto;
 import com.example.demo.service.tmm.TMMA0011ServiceImpl;
 
 @WebMvcTest(TMMA0011Controller.class)
@@ -32,17 +32,18 @@ public class TMMA0011ControllerTest {
     @DisplayName("공통코드 조회 테스트")
     void getTmCodexhTest() throws Exception {
 
-        TmCodexh tmCodexh = new TmCodexh();
-        tmCodexh.setId("1");
-        tmCodexh.setCOMM_CODE("ACCT_CODE");
-        tmCodexh.setCOMM_CDNM("계정과목코드");
-        tmCodexh.setSYST_CODE("PAY");
+        // TODO: JWT Authentication
 
-        given(tmma0011service.getTmCodexh("ACCT_CODE", "PAY")).willReturn(Arrays.asList(tmCodexh));
-        mockMvc.perform(get("/api/TMMA0011/TmCodexh?COMM_CDNM=ACCT_CODE&SYST_CODE=PAY")).andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").exists()).andExpect(jsonPath("$[0].COMM_CODE").exists())
-                .andExpect(jsonPath("$[0].COMM_CDNM").exists()).andExpect(jsonPath("$[0].SYST_CODE").exists())
+        CommonCodeHeaderResponseDto cchrDto = new CommonCodeHeaderResponseDto();
+        cchrDto.setCommonCode("ACCT_CODE");
+        cchrDto.setCommonCodeName("계정과목코드");
+        cchrDto.setSystemCode("PAY");
+
+        given(tmma0011service.getCommonCodeHeaders("ACCT_CODE", "PAY")).willReturn(Arrays.asList(cchrDto));
+        mockMvc.perform(get("/api/TMMA0011/commonCodeHeader?COMM_CDNM=ACCT_CODE&SYST_CODE=PAY")).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].commonCode").exists()).andExpect(jsonPath("$[0].commonCodeName").exists())
+                .andExpect(jsonPath("$[0].systemCode").exists())
                 .andDo(print());
-        verify(tmma0011service).getTmCodexh("ACCT_CODE", "PAY");
+        verify(tmma0011service).getCommonCodeHeaders("ACCT_CODE", "PAY");
     }
 }
