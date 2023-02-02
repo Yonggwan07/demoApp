68546c7c72091a0cabf0fbc4058c738be0ecb4c3
Park Yonggwan <pyk@inbus.co.kr>
2023-02-02 15:22:52 +0900

    UserAuditor 사용자 아이디 authentication 통해 가져오도록 수정

--- src/main/java/com/example/demo/UserAuditorAware.java
+++ src/main/java/com/example/demo/UserAuditorAware.java
@@ -3,31 +3,24 @@ package com.example.demo;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.example.demo.data.dto.cmm.UserResponseDto;
-import com.example.demo.security.JwtTokenProvider;
-import com.example.demo.service.cmm.ComUserService;
+import com.example.demo.data.entity.cmm.User;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
 public class UserAuditorAware implements AuditorAware<String> {
-
-    private final JwtTokenProvider jwtTokenProvider;
-    private final ComUserService comUserService;
-
     @Override
     public Optional<String> getCurrentAuditor() {
-        
-        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-        String token = jwtTokenProvider.resolveToken(attr.getRequest());
-        String userId = jwtTokenProvider.getUserId(token);
-        UserResponseDto userResponseDto = comUserService.getUserInfo(userId);
-
-        return Optional.of(userResponseDto.getUserId());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (null == authentication || !authentication.isAuthenticated()) {
+            return null;
+        }
+        User user = (User) authentication.getPrincipal();
+        return Optional.of(user.getUserId());
     }
 }
